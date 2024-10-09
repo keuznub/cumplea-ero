9d91c4c14bd4a66cc36f6672e50d14859f04aa16
Keuznub <breixopastoriza@gmail.com>
2024-10-09 10:43:25 +0200

    Segunda version faltan cosas js y mucho css

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import dayjs from 'dayjs'
+dayjs().format()
 
+const date = document.querySelector(".date")
+const result = document.querySelector(".result")
+date.addEventListener("change", (e)=>onchange(e))
+console.log("fuera de onchange")
+
+const onchange = (e) => {
+    result.textContent = dayjs(date.value).fromNow()
+}
 
